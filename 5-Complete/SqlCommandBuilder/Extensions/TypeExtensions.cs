7d97c52092afe651ae73e268daa89bc0828902d4
Vagif Abilov <vagif.abilov@gmail.com>
2018-11-06 20:33:13 +0100

    Ported to .NETStandard/.NETCore.

--- 5-Complete/SqlCommandBuilder/Extensions/TypeExtensions.cs
+++ 5-Complete/SqlCommandBuilder/Extensions/TypeExtensions.cs
@@ -1,72 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 
 namespace SqlCommandBuilder
 {
     public static class TypeExtensions
     {
-#if NET40 || SILVERLIGHT || PORTABLE_LEGACY
-        public static IEnumerable<PropertyInfo> GetDeclaredProperties(this Type type)
-        {
-            return type.GetProperties();
-        }
-
-        public static PropertyInfo GetDeclaredProperty(this Type type, string propertyName)
-        {
-            return type.GetProperty(propertyName);
-        }
-
-        public static IEnumerable<FieldInfo> GetDeclaredFields(this Type type)
-        {
-            return type.GetFields();
-        }
-
-        public static MethodInfo GetDeclaredMethod(this Type type, string methodName)
-        {
-            return type.GetMethod(methodName, BindingFlags.Instance | BindingFlags.NonPublic);
-        }
-
-        public static IEnumerable<ConstructorInfo> GetDeclaredConstructors(this Type type)
-        {
-            return type.GetConstructors(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
-        }
-
-        public static ConstructorInfo GetDefaultConstructor(this Type type)
-        {
-            return type.GetConstructor(new Type[] {});
-        }
-
-        public static TypeAttributes GetTypeAttributes(this Type type)
-        {
-            return type.Attributes;
-        }
-
-        public static Type[] GetGenericTypeArguments(this Type type)
-        {
-            return type.GetGenericArguments();
-        }
-
-        public static bool IsTypeAssignableFrom(this Type type, Type otherType)
-        {
-            return type.IsAssignableFrom(otherType);
-        }
-
-        public static bool HasCustomAttribute(this Type type, Type attributeType, bool inherit)
-        {
-            return Attribute.IsDefined(type, attributeType, inherit);
-        }
-
-        public static bool IsGeneric(this Type type)
-        {
-            return type.IsGenericType;
-        }
-
-        public static bool IsValue(this Type type)
-        {
-            return type.IsValueType;
-        }
-#else
         public static IEnumerable<PropertyInfo> GetDeclaredProperties(this Type type)
         {
             return type.GetTypeInfo().DeclaredProperties;
@@ -126,6 +66,5 @@ namespace SqlCommandBuilder
         {
             return type.GetTypeInfo().IsValueType;
         }
-#endif
     }
 }
